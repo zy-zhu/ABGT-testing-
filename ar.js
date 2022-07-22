1ae4de50b4d750d3576a04f6a895530121fd4bd3
ZoeyZZZ <50037741+zy-zhu@users.noreply.github.com>
2022-07-21 22:42:37 -0700

    push the quiz

--- ar.js
+++ ar.js
@@ -1,11 +1,20 @@
-function closeWindow(){
-    var modal=document.getElementById('myModal');
-    var span=document.getElementsByClassName('close')[0];
 
-    span.onclick=function(){
-        modal.style.display='none';
-    }
-    window.onclick=function(event){
-        if(event.target==modal)modal.style.display='none';
-    }
+
+
+
+
+function closeWindow(){
+
+   var modal=document.getElementById('myModal');
+   var span=document.getElementById('close');
+    modal.style.display='none';
+
+    // span.onclick=function(){
+    //     modal.style.display='none';
+    // }
+
+    // window.onclick=function(event){
+    //     if(event.target==modal)modal.style.display='none';
+    // }
+    console.log('helohedhajkdhas')
 }
