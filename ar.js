8e3465089572c727bba26df803ad455d2cde36a5
ZoeyZZZ <50037741+zy-zhu@users.noreply.github.com>
2022-07-22 17:34:00 -0700

    ar installation change

--- ar.js
+++ ar.js
@@ -7,7 +7,11 @@ function closeWindow(){
 
    var modal=document.getElementById('myModal');
    var span=document.getElementById('close');
+   var instruction=document.getElementById('instruction');
+
+
     modal.style.display='none';
+    instruction.style.display='inline';
 
     // span.onclick=function(){
     //     modal.style.display='none';
@@ -16,5 +20,36 @@ function closeWindow(){
     // window.onclick=function(event){
     //     if(event.target==modal)modal.style.display='none';
     // }
-    console.log('helohedhajkdhas')
+ 
+}
+
+function openIntro(){
+
+    var introbutton=document.getElementById('intro');
+    var introvideo=document.getElementById('video-intro');
+    var modal=document.getElementById('myModal');
+    modal.style.display='none';
+    introvideo.style.display='inline';  
+
+}
+
+function closeVideo(){
+    var skip=document.getElementById('skip');
+    var introvideo=document.getElementById('video-intro');
+    var instruction=document.getElementById('instruction');
+
+    introvideo.style.display='none';  
+    instruction.style.display='inline';
+}
+
+
+
+function openInstru(){
+    var instruction=document.getElementById('instruction');
+    var introvideo=document.getElementById('video-intro');
+
+    introvideo.style.display='inline';  
+    instruction.style.display='none';
+
+    
 }
